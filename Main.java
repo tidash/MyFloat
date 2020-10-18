3e68736ebff69cc399b79176829e7608da0c0b46
tidash <72913691+tidash@users.noreply.github.com>
2020-10-18 22:52:42 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+package com.jetbrains;
+
+public class Main {
+
+    public static void main( String[] args) {
+
+        MyFloat a = new MyFloat(504, 2);
+        MyFloat b = new MyFloat(1004, 3);
+        a.sum(b);
+        a.dif(b);
+    }
+}
